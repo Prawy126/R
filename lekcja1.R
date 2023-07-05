3885b5e8aa6b9943e98f8a5344c06367fd88e4db
Prawy_126 <michal190713@gmail.com>
2023-07-05 14:04:57 +0200

    podstawy R

---
+++ lekcja1.R
@@ -0,0 +1,27 @@
+print("Hello World!")
+cat("Hello World\nWitaj świecie")
+
+help(print)
+?print
+?cat
+
+args(print)
+
+zmienna1 <- 1L
+zmienna1
+print(zmienna1)
+
+zmienna2 <- 1.0
+zmienna2
+print(zmienna2)
+typeof(zmienna1);mode(zmienna1)
+typeof(zmienna2);mode(zmienna2)
+
+(zmienna3 <-1)
+print(zmienna3)
+zmienna3
+typeof(zmienna3);mode(zmienna3)
+
+str(zmienna3)
+# komentarz w R
+# ctrl+shift+c zamienia linię na komentarz
